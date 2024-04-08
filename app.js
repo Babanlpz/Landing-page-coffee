74cd7fd889b1f8ec64e517fa9d4a02557d862ff0
Esteban <baab2an@live.fr>
2024-04-08 16:41:15 +0200

    Feat landing page COFFEE

---
+++ app.js
@@ -0,0 +1,32 @@
+const search = document.querySelector(".search-container");
+const searchBtn = document.querySelector("#search-icon");
+const nav = document.querySelector("nav");
+const btnBurger = document.querySelector("#burger");
+const navbar = document.querySelector(".navbar");
+const links = document.querySelectorAll(".navbar a");
+
+searchBtn.addEventListener("click", () => {
+  search.classList.toggle("active");
+  navbar.classList.remove("active");
+});
+
+btnBurger.addEventListener("click", () => {
+  navbar.classList.toggle("active");
+  search.classList.remove("active");
+});
+
+window.addEventListener("scroll", () => {
+  nav.classList.toggle("active", window.scrollY > 0);
+});
+
+window.addEventListener("scroll", () => {
+  search.classList.remove("active");
+  navbar.classList.remove("active");
+});
+
+links.forEach((link) => {
+  link.addEventListener("click", () => {
+    search.classList.remove("active");
+    navbar.classList.remove("active");
+  });
+});
